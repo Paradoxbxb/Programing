03cb0ce1196fb38a4111eafd6d652b186d852062
BRUNO CUNHA UCHOA <04053365@sempreunama.com.br>
2019-09-03 10:06:47 -0300

    Aula03

---
+++ src/com/unama/Main.java
@@ -0,0 +1,10 @@
+package com.unama;
+public class Main {
+
+    public static void main(String[] args) {
+        String str1 = "First";
+        String str2 = "Second";
+
+        System.out.println("This is the first");
+    }
+}
