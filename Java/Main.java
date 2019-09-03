25df77660d746ba8dafc370b4f8b9973a7624eea
Paradox <51055432+Paradoxbxb@users.noreply.github.com>
2019-09-03 10:25:33 -0300

    Add files via upload

---
+++ Java/Main.java
@@ -0,0 +1,21 @@
+package com.unama;
+import javafx.scene.layout.HBox;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalCopias;
+        System.out.println("informe o total de copias: ");
+        totalCopias =sc.nextInt();
+        double totalPago;
+        if(totalCopias <= 100) {
+            totalPago = totalCopias * 0.25;
+        }else(
+                totalPago = totalCopias * 0.20;
+    }
+    System.out.println ()
+
+
+}
